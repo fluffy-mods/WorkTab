fcb8f905727947e2ad4272045145cb1c26281639
Fluffy <fluffy.l2032@gmail.com>
2018-04-04 12:26:38 +0200

    'Release v0.18.1.7 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("18.1.6.126")]
-[assembly: AssemblyFileVersion("18.1.6.126")]
+[assembly: AssemblyVersion("18.1.7.127")]
+[assembly: AssemblyFileVersion("18.1.7.127")]
