3f2403839ca288dd6cc25b056e89cadd30054e8b
Fluffy <fluffy.l2032@gmail.com>
2018-10-06 21:55:30 +0200

    'Release 2.13.167 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.12.166")]
-[assembly: AssemblyFileVersion("2.12.166")]
+[assembly: AssemblyVersion("2.13.167")]
+[assembly: AssemblyFileVersion("2.13.167")]
