1c31c2fbcdb48ff4138a8517222460857d8e8aba
Fluffy <fluffy.l2032@gmail.com>
2018-10-06 21:53:37 +0200

    patch WorkSettings_DisableAll to fix bugs with some custom pawn type mods

---
+++ Source/HarmonyPatches/Worksettings/Pawn_WorkSettings_DisableAll.cs
@@ -0,0 +1,17 @@
+﻿// Pawn_WorkSettings_DisableAll.cs
+// Copyright Karel Kroeze, 2018-2018
+
+using Harmony;
+using RimWorld;
+
+namespace WorkTab
+{
+    [HarmonyPatch( typeof( Pawn_WorkSettings ), "DisableAll" )]
+    public class Pawn_WorkSettings_DisableAll
+    {
+        public static void Prefix( Pawn_WorkSettings __instance )
+        {
+            __instance.Pawn().DisableAll();
+        }
+    }
+}
