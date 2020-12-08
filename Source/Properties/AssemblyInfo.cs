36b66ef8b84ffba7b9b82e5c18e407425608bccf
Fluffy <fluffy.l2032@gmail.com>
2020-12-08 19:04:43 +0100

    Release 3.21.307 [nolog]

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.21.303")]
+[assembly: AssemblyFileVersion("3.21.307")]
