4e8ba7a9c096aa3be4ca25551ece73604cb223db
FluffierThanThou <fluffy.l2032@gmail.com>
2021-07-22 22:08:05 +0200

    release v4.1.316

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("4.0.315")]
+[assembly: AssemblyFileVersion("4.1.316")]
